dc4d38c8e885b6af7eb5370a186af5c782be5dee
Nikolai <magerk38@gmail.com>
2023-02-10 22:16:17 +0800

    jstcmt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Watches } from "./components/Watches";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+const zones = require('./zones.json')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [clocks, setClacks] = useState([
+        { name: "Grinvich", timezone: 0, id: uuidv4() },
+    ]);
+    const [newClock, setNewClock] = useState({});
+    //console.log(zones)
+   
+    const handleChange = (e) => {
+        setNewClock({
+            ...newClock,
+            [e.target.name]: e.target.value,
+            id: uuidv4(),
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setClacks([...clocks, newClock]);
+        document.getElementById('nameInput').value = '';
+        
+    };
+
+    const handleDelete = (e) => {
+        setClacks(clocks.filter(clock => clock.id !== e.target.id))
+    }
+
+   
+
+    return (
+        <div className="App">
+            <form>
+                <input id='nameInput' name="name" type="select" onChange={handleChange} ></input>
+                <select name="timezone" onChange={handleChange} > 
+                    {zones.map(zone => <option value={zone.value}>{zone.name}</option>)}   
+                </select>
+                
+                    
+                
+                <button onClick={handleSubmit}>Добавить</button>
+            </form>
+            {clocks.map((clock) => (
+                <Watches
+                    timezone={clock.timezone}
+                    id={clock.id}
+                    name={clock.name}
+                    handleDelete={handleDelete}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Watches.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+
+export const Watches = ({ timezone, id, name, handleDelete }) => {
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const now = new Date();
+           // console.log(now)
+            const offset = new Date().getTimezoneOffset();
+            //console.log(offset);
+
+            const hours = now.getHours() + Number(timezone) + (offset/60);
+           // console.log(hours)
+            const minutes = now.getMinutes();
+            const seconds = now.getSeconds();
+
+            const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30;
+            const minuteDegrees = (minutes / 60) * 360 + (seconds / 60) * 6;
+            const secondDegrees = (seconds / 60) * 360;
+
+            document.getElementById(
+                `${id}hour`
+            ).style.transform = `rotate(${hourDegrees}deg)`;
+            document.getElementById(
+                `${id}min`
+            ).style.transform = `rotate(${minuteDegrees}deg)`;
+            document.getElementById(
+                `${id}sec`
+            ).style.transform = `rotate(${secondDegrees}deg)`;
+        }, 1000);
+
+        return () => clearInterval(intervalId)
+    }, []);
+
+    return (
+        <div className="clock-container">
+            <h1 className="name">{name}</h1>
+            <div className="clock" id={id + "clock"}>
+                <div className="hour-hand" id={id + "hour"}></div>
+                <div className="minute-hand" id={id + "min"}></div>
+                <div className="second-hand" id={id + "sec"}></div>
+            </div>
+            <button id={id} onClick={handleDelete}> X </button>
+        </div>
+    );
+};
